b35ddee95a58421c81bce113f99dfeaf87aee9cf
aAlejandroz <azalejandro1@yahoo.com>
2019-07-11 21:33:58 -0500

    Small refactoring

--- bot.js
+++ bot.js
@@ -1,57 +1,45 @@
 console.log("Bot is starting");
 var Twit = require('twit');
 var config = require('./config');
-//console.log(config);
+
+
 var T = new Twit(config);
 var fs = require('fs');
 var files = fs.readdirSync('memes');
 console.log(files);
 
-
 var iterator = 0;
-
-var myvar = setInterval(TweetIt, 1800000)
-
-myvar;
 
 function TweetIt()
 {
-
 	var filename = "memes/" + files[iterator];
 	iterator++;
 
 	var b64content = fs.readFileSync(filename, {encoding: 'base64'})
 
-	T.post('media/upload',{media_data: b64content}, uploaded);
-
-	//T.post('statuses/update', tweet, respon);
-	function uploaded(err,data,response){
+	T.post('media/upload',{media_data: b64content}, (err,data,response) =>{
 		var id = data.media_id_string;
 		var tweet = {
 			status : "",
 			media_ids: [id]
 		}
-			T.post("statuses/update",tweet,respon)
-	}
+		T.post("statuses/update",tweet,(err, data, response) =>{
+			if (err)
+			{
+				console.log(err);
+			}
+			else{
+				console.log("Tweet Sent");
+			}
+		});
+	});
 
-	function respon(err, data, response)
-	{
-		if (err)
-		{
-			console.log("Error!");
-		}
-		else{
-			console.log("Tweet Sent");
-		}
-	}
-	// Ensures the bot will end functionality once there are no more files to upload from a folder
 	if(iterator == files.length)
 	{
 		clearInterval(myvar);
 	}
 }
 
+var myvar = setInterval(TweetIt, 3000)
 
-
-
-//TweetIt();
+myvar;
